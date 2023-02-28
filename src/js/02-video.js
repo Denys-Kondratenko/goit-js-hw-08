8925450ec454ec6312569d5157189a8f6c9a301a
Denys-Kondratenko <dk132652@gmail.com>
2023-02-28 22:50:04 +0200

    зробив 2 та 3 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    console.log(data);
+  }, 1000)
+);
+
+const getLocalValue = localStorage.getItem('videoplayer-current-time');
+try {
+  const getObjectValue = JSON.parse(getLocalValue);
+  console.log(getObjectValue.seconds);
+
+  player.setCurrentTime(getObjectValue.seconds);
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formFeedback = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form [name="email"]');
+const message = document.querySelector('.feedback-form [name="message"]');
+
+let formData = {};
+
+function setFeedback(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+formFeedback.addEventListener(
+  'input',
+  throttle(e => {
+    setFeedback(e);
+  }, 500)
+);
+
+function getFeedback() {
+  const savedFeedback = localStorage.getItem('feedback-form-state');
+  if (savedFeedback) {
+    const parsedFeedback = JSON.parse(savedFeedback);
+
+    formData = parsedFeedback;
+
+    email.value = parsedFeedback.email || '';
+    message.value = parsedFeedback.message || '';
+  }
+}
+
+formFeedback.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log('submit');
+  console.log(formData);
+
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+});
+
+getFeedback();
